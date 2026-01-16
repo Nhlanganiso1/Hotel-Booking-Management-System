ecbd7da82edb4fd38cb276dea9a9d20cf12aee87
Nhlanganiso1 <nhlanganisositholejobe@gmail.com>
2026-01-17 00:46:51 +0200

    Changed the name of form1 to login page, created folders to hold pages for our respective users

--- Login Page.Designer.cs
+++ Login Page.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Hotel_Booking_Management_System
 {
-    partial class Form1
+    partial class LoginForm
     {
         /// <summary>
         /// Required designer variable.
@@ -30,13 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // LoginForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(890, 470);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(890, 467);
+            this.Name = "LoginForm";
+            this.Text = "Booking Management";
             this.ResumeLayout(false);
 
         }
